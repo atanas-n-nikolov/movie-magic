1ea3a3064ca4d45893e2d74084f95ab5468bd3ee
atanas-n-nikolov <156112757+atanas-n-nikolov@users.noreply.github.com>
2024-10-06 14:16:01 +0300

    Test hbs layout

--- src/config/mongooseInit.js
+++ src/config/mongooseInit.js
@@ -1,6 +1,6 @@
 import { connect } from "mongoose";
 
-const dbUrl = "mongodb://localhost:27017/";
+const dbUrl = "mongodb://localhost:27017/Test";
 
 export default async function mongooseInit() {
   try {
--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import express from "express";
 import handlebarsInit from "./config/handlebarsInit.js";
 import expressInit from "./config/expressInit.js";
 import mongooseInit from "./config/mongooseInit.js";
+import routes from "./routes.js";
 const app = express();
 
 mongooseInit();
 handlebarsInit(app);
 expressInit(app);
 
-app.get('/', (req, res) => {
-  res.render('movies/home');
-});
+app.use(routes);
 
 app.listen(5000, () => console.log('Server is listening on port: http://localhost:5000 ...'));
