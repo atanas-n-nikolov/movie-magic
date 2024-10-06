0f0fc5d06af54228072e56b67bca7cc0017d4288
atanas-n-nikolov <156112757+atanas-n-nikolov@users.noreply.github.com>
2024-10-06 12:56:53 +0300

    Setting express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('It works');
+import express from "express";
+import mongoose from "mongoose";
+import handlebars from 'express-handlebars';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('It works!');
+});
+
+app.listen(5000, () => console.log('Server is listening on port: http://localhost:5000 ...'));
