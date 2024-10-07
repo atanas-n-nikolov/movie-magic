499fd1a47589801044c0d70b5ef9defa90008f77
atanas-n-nikolov <156112757+atanas-n-nikolov@users.noreply.github.com>
2024-10-07 17:49:46 +0300

    Get movies from db

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
+import movieService from "../services/movieService.js";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.render('home');
+function toArray(documents) {
+  return documents.map(document => document.toObject());
+};
+
+router.get('/', async (req, res) => {
+  const movies = await movieService.getAll();
+  console.log(movies);
+  res.render('home', { movies: toArray(movies) });
 });
 
 router.get('/about', (req, res) => {
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,7 +1,14 @@
 import Movie from "../models/Movie.js";
 
+const getAll = async (filter = {}) => {
+  let moviesQuery = await Movie.find();
+
+  return moviesQuery;
+};
+
 const create = (movie) =>  Movie.create(movie);
 
 export default {
+  getAll,
   create,
 }
