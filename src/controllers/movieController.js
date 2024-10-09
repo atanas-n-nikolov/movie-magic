44edc8acd4d482829e7d22c041f0708939085f6e
atanas-n-nikolov <156112757+atanas-n-nikolov@users.noreply.github.com>
2024-10-09 13:50:22 +0300

    Add create view and render

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -2,29 +2,25 @@ import { Router } from "express";
 import movieService from "../services/movieService.js";
 const router = Router();
 
-router.get('/movies/create', (req, res) => {
+router.get('/create', (req, res) => {
   res.render('movies/create');
 });
 
-router.post('/movies/create', async (req, res) => {
+router.post('/create', async (req, res) => {
   await movieService.create(req.body);
   res.redirect('/');
 });
 
-router.get('/movies/details/:id', async (req, res) => {
+router.get('/details/:id', async (req, res) => {
   const id = req.params.id;
   const movie = await movieService.getOne(id).lean();
   res.render('movies/details', { movie });
 });
 
-router.get('/movies/search', async (req, res) => {
+router.get('/search', async (req, res) => {
   const filter = req.query;
   const movies = await movieService.getAll(filter).lean();
   res.render('home', { isSearch: true, movies, filter });
-});
-
-router.get('*', (req, res) => {
-  res.render('home/404');
 });
 
 export default router;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import homeController from './controllers/homeController.js';
 import movieController from './controllers/movieController.js';
+import castController from './controllers/castController.js';
 
 const router = Router();
 router.use(homeController);
-router.use(movieController);
+router.use('/movies', movieController);
+router.use('/casts', castController);
+
+router.all('*', (req, res) => {
+  res.render('home/404');
+});
 
 export default router;
